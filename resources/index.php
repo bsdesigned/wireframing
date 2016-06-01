2f2d79bde64815c794c5bfd17702125c186f33ce
Brent Starling <BrentStarling@rentalcars.com>
2016-06-01 10:35:41 +0100

    new files

--- resources/index.php
+++ resources/index.php
@@ -8,15 +8,16 @@
   <meta name="description" content="Wireframe Layout">
   <meta name="author" content="BSD">
 
-  <link rel="stylesheet" href="css/main.css?v=1.0">
   <link rel="stylesheet" href="css/modules.min.css?v=1.0">
+  <link rel="stylesheet" href="css/main.css?v=1.0">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
 
   <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 </head>
 
-<body>
+<body class="styles">
 
   <section class="header">
     <div class="container">
@@ -26,14 +27,74 @@
     </div>
   </section>
 
-  <section class="main-block">
+  <section class="content">
     <div class="container">
+
       <div class="columns">
-        <div class="column col-6">col-6</div>
-        <div class="column col-3">col-3</div>
-        <div class="column col-2">col-2</div>
-        <div class="column col-1">col-1</div>
+        <div class="column col-12">
+          <h1>First Colour</h1>
+          <h2>$iao-first: #B54077;</h2>
+          <span class="colour-block a1">1</span>
+          <span class="colour-block a2">2</span>
+          <span class="colour-block a3">3</span>
+          <span class="colour-block a4">4</span>
+          <span class="colour-block a5">5</span>
+          <span class="colour-block a6">6</span>
+        </div>
       </div>
+
+      <div class="columns">
+        <div class="column col-12">
+          <h1>Second Colour</h1>
+          <h2>$iao-second: #A80A3D;</h2>
+          <span class="colour-block b1">1</span>
+          <span class="colour-block b2">2</span>
+          <span class="colour-block b3">3</span>
+          <span class="colour-block b4">4</span>
+          <span class="colour-block b5">5</span>
+          <span class="colour-block b6">6</span>
+        </div>
+      </div>
+
+      <div class="columns">
+        <div class="column col-12">
+          <h1>Third Colour</h1>
+          <h2>$iao-third: #1F3447;</h2>
+          <span class="colour-block c1">1</span>
+          <span class="colour-block c2">2</span>
+          <span class="colour-block c3">3</span>
+          <span class="colour-block c4">4</span>
+          <span class="colour-block c5">5</span>
+          <span class="colour-block c6">6</span>
+        </div>
+      </div>
+
+      <div class="columns">
+        <div class="column col-12">
+          <h1>Forth Colour</h1>
+          <h2>$iao-fourth: #736966;</h2>
+          <span class="colour-block d1">1</span>
+          <span class="colour-block d2">2</span>
+          <span class="colour-block d3">3</span>
+          <span class="colour-block d4">4</span>
+          <span class="colour-block d5">5</span>
+          <span class="colour-block d6">6</span>
+        </div>
+      </div>
+
+      <div class="columns">
+        <div class="column col-12">
+          <h1>Fifth Colour</h1>
+          <h2>$iao-fifth: #FFF9D7;</h2>
+          <span class="colour-block e1">1</span>
+          <span class="colour-block e2">2</span>
+          <span class="colour-block e3">3</span>
+          <span class="colour-block e4">4</span>
+          <span class="colour-block e5">5</span>
+          <span class="colour-block e6">6</span>
+        </div>
+      </div>
+
     </div>
   </section>
 
---
+++ resources/sign-up.php
@@ -0,0 +1,45 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Wireframe Layout</title>
+  <meta name="description" content="Wireframe Layout">
+  <meta name="author" content="BSD">
+
+  <link rel="stylesheet" href="css/main.css?v=1.0">
+  <link rel="stylesheet" href="css/modules.min.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+  <section class="header">
+    <div class="container">
+      <div class="columns">
+        <div class="column col-12">Header</div>
+      </div>
+    </div>
+  </section>
+
+  <section class="main-block">
+    <div class="container">
+      <div class="columns">
+        <div class="column col-12">Main</div>
+      </div>
+    </div>
+  </section>
+
+  <section class="footer">
+    <div class="container">
+      <div class="columns">
+        <div class="column col-12">Footer</div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
